6275e079748987c620b2071f36b26dcc122d7eaa
optimalchief <velazquez234@yahoo.com>
2022-03-30 12:51:00 -0400

    Adding changes to repositories

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -14,11 +14,15 @@ import requests
 
 def main(dict):
     databaseName = "dealerships"
-
+    secret = {
+        "COUCH_URL":"https://c8eb5a3f-130c-4cd1-b168-14aa6ad3da10-bluemix.cloudantnosqldb.appdomain.cloud",
+        "IAM_API_KEY":"Z90P54UqWQ8qulUEc0NOkDCWzi_XJrQYEvDQJqCUwOAD",
+        "COUCH_USERNAME":"c8eb5a3f-130c-4cd1-b168-14aa6ad3da10-bluemix",
+    }
     try:
         client = Cloudant.iam(
-            account_name=dict["COUCH_USERNAME"],
-            api_key=dict["IAM_API_KEY"],
+            account_name=secret["COUCH_USERNAME"],
+            api_key=secret["IAM_API_KEY"],
             connect=True,
         )
         print("Databases: {0}".format(client.all_dbs()))
